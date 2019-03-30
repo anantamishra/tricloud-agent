9183071814065a244bd8335d5c89a65bd62c11d2
bornjre <bornjre@gmail.com>
2019-03-31 02:45:36 +0545

    moved common stuff to server

--- commands/commands.go
+++ commands/commands.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 
+	"github.com/indrenicloud/tricloud-server/broker/core"
 	"github.com/shirou/gopsutil/mem"
 )
 
@@ -15,12 +16,12 @@ func init() {
 type CommandFunc func(args ...string) string
 
 // CommandBuffer contain the mapping of different command type to their mapping (func)
-var CommandBuffer map[CommandType]CommandFunc
+var CommandBuffer map[core.CommandType]CommandFunc
 
 // all commands will be registered from here
 func registerCommands() {
 	// internal commands
-	CommandBuffer[CMD_SYSTEM_INFO] = SystemInfo
+	CommandBuffer[core.CMD_SYSTEM_INFO] = SystemInfo
 }
 
 // SystemInfo gives system info of machine
