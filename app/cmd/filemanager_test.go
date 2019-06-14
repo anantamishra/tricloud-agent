fe255717a38961f3db7043812aa00ecd13e3e60a
bornjre <bornjre@gmail.com>
2019-06-15 01:51:41 +0545

    added a test for COPY

--- app/cmd/filemanager_test.go
+++ app/cmd/filemanager_test.go
@@ -11,3 +11,29 @@ func TestList(t *testing.T) {
 	ll := listDirectory("/")
 	p(ll)
 }
+
+func TestCopy(t *testing.T) {
+
+	ff := &FmActionReq{
+		Action:      "copy",
+		Basepath:    "/home/bing/golang/two/src/github.com/indrenicloud/tricloud-agent/app/cmd/testfolder/a",
+		Targets:     []string{"targetfile"},
+		Destination: "/home/bing/golang/two/src/github.com/indrenicloud/tricloud-agent/app/cmd/testfolder/b",
+	}
+
+	ffres := actionCopy(ff)
+
+	t.Logf("%+v", ffres)
+
+	ff2 := &FmActionReq{
+		Action:      "copy",
+		Basepath:    "/home/bing/golang/two/src/github.com/indrenicloud/tricloud-agent/app/cmd/testfolder/a",
+		Targets:     []string{"c"},
+		Destination: "/home/bing/golang/two/src/github.com/indrenicloud/tricloud-agent/app/cmd/testfolder/b",
+	}
+
+	ffres2 := actionCopy(ff2)
+
+	t.Logf("%+v", ffres2)
+
+}
