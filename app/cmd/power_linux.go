1aa8ddbf103ec4e597b9c4cf09217295c945025f
bornjre <bornjre@gmail.com>
2019-06-16 21:56:41 +0545

    switchroo

--- app/cmd/power_linux.go
+++ app/cmd/power_linux.go
@@ -8,14 +8,14 @@ import (
 
 func doPoweroff() {
 	//err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF)
-	if err := exec.Command("systemctl", "reboot", "-i").Run(); err != nil {
+	if err := exec.Command("systemctl", "poweroff").Run(); err != nil {
 		logg.Debug(err)
 	}
 }
 
 func doReboot() {
 	//err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART)
-	if err := exec.Command("systemctl", "shutdown", "-i").Run(); err != nil {
+	if err := exec.Command("systemctl", "reboot").Run(); err != nil {
 		logg.Debug(err)
 	}
 
